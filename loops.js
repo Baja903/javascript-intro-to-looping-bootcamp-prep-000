3b9674d47c7f88ab7cc34e3e70afedc3ebbdecc9
rational-dispatch-0384 <baja903@gmail.com>
2019-04-24 18:40:28 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,8 +18,14 @@ function whileLoop(n) {
     return "done";
 }
 
-function doWhileLoop(array) {
-  do {array.pop()}
-  while (array.length > 0 && maybeTrue());
-  return array
+function doWhileLoop(array){
+function maybeTrue() {
+return Math.random() >= 0.5
+}
+
+do {
+array.pop();
+} while (array.length > 0 &&maybeTrue());
+return array;
+
 }
