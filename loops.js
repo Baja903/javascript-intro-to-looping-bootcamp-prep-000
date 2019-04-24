b411803f8b8fba4f1f8f88b5ffaa09ff38f8f110
rational-dispatch-0384 <baja903@gmail.com>
2019-04-24 23:08:20 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,29 +1,37 @@
-function forLoop(array) {
-  for ( let i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.push(`I am 1 strange loop.`);
-    } else {
-      array.push(`I am ${i} strange loops.`);
-    }
-  }
-  return array;
+
+  
+  
+  function forLoop(array){
+  for (let i = 0; i < 25; i++) {
+ if (i === 1) {
+ array.push(`I am 1 strange loop.`);
+ } 
+ else {
+ array.push(`I am ${i} strange loops.`);
+}
+}
+return array;
 }
 
-function whileLoop(n) {
-  let countdown = n;
-  
-    while (countdown > 0) {
-      console.log(--countdown)
-    }
-    return "done";
+
+function whileLoop(number){
+  let countdown = number;
+
+while (countdown > 0) {
+  console.log(--countdown);
+}
+return "done"
 }
 
-function doWhileLoop(num){
-    var i = 0;
-    do{
-      console.log("I run once regardless.");
-      i++;
-    } while (i < 0);
+  var i = -1;
+function doWhileLoop(num) {
+  function incrementVariable() {
+    i=i+1;
+    return i
+   }
 
-  }
-  
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+
+}
